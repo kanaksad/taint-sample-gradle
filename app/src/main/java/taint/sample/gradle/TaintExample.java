f556f2e0a7ca4563590153c56cdb154957a58147
anoop-pn <pn.anoop5@gmail.com>
2022-11-18 13:46:31 -0800

    integrating ucr taint checker

--- app/src/main/java/taint/sample/gradle/TaintExample.java
+++ app/src/main/java/taint/sample/gradle/TaintExample.java
@@ -1,15 +1,15 @@
 package gradle;
 
-import org.checkerframework.checker.tainting.qual.Tainted;
+import edu.ucr.cs.riple.taint.ucrtainting.qual.RTainted;
 import java.io.IOException;
 
 public class TaintExample {
 
-    void makeExternalCall(@Tainted String s) {
+    void makeExternalCall(@RTainted String s) {
         TaintStubSample.printUntaintedString(s);
     }
 
-    void testRCE(@Tainted String s) throws IOException {
+    void testRCE(@RTainted String s) throws IOException {
         Runtime.getRuntime().exec(s);
     }
 
